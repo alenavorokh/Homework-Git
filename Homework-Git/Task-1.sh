96a7d0fb91e36d50d9c68bdd40fcb6486293aa92
alenavorokh <98804890+alenavorokh@users.noreply.github.com>
2022-02-16 21:15:35 +0200

    Change format data to a

--- Homework-Git/Task-1.sh
+++ Homework-Git/Task-1.sh
@@ -1,8 +1,8 @@
 #Task-3
 #!/bin/bash
-day="$(date "+%A")"
+day="$(date "+%a")"
 number="$(date "+%d")"
-if [[ "$day" == "$Saturday" || "$day" == "$Sunday" ]]; then
+if [[ "$day" == "$Sat" || "$day" == "$Sun" ]]; then
         echo "Looks like ${number} is a weekend."
 else 
         echo "Looks like ${number} is a working day."
